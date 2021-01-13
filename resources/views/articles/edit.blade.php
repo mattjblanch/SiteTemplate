da4693f3a2a99423546a31d114d36ad075390f79
Matt Blanch <mattjblanch@gmail.com>
2021-01-13 22:11:03 +1100

    After ep 24 - Forms That Submit PUT Requests

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -7,9 +7,9 @@
         <div id="page" class="container">
             <h1 class="heading has-text-weight-bold is-size-4">Update Article</h1>
 
-            <form method="POST" action="/articles">
+            <form method="POST" action="/articles/{{$article->id}}">
                 @csrf
-
+                @method('PUT')
                 <div class="field">
                     <label for="title" class="label">Title</label>
 
